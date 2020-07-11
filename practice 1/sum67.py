f7881aa69a4b1d4e78c30ebe3b72d8cf7b5a1dd8
itshendson <hendsonszeto@live.com>
2020-07-10 19:21:32 -0700

    sum list except integers between 6 and 7

---
+++ practice 1/sum67.py
@@ -0,0 +1,25 @@
+# Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers.
+#
+# sum67([1, 2, 2]) → 5
+# sum67([1, 2, 2, 6, 99, 99, 7]) → 5
+# sum67([1, 1, 6, 7, 2]) → 4
+# sum67([2, 7, 6, 2, 6, 7, 2, 7]) → 18
+
+def sum67(nums):
+    list_nums = []
+    index_nums = 0
+
+
+    while index_nums < len(nums):
+
+        if nums[0 + index_nums] != 6:
+            list_nums.append(nums[0 + index_nums])
+            index_nums += 1
+
+        elif nums[0 + index_nums] == 6:
+            while nums[0 + index_nums] != 7:
+                index_nums += 1
+            index_nums += 1
+
+    print(sum(list_nums))
+    return sum(list_nums)
