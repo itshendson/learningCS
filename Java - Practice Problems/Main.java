7c77b7500db987a53ae097c7966b22d349b4bcf8
itshendson <hendsonszeto@live.com>
2020-08-06 17:56:05 -0700

    java stuff

--- Java - Practice Problems/Main.java
+++ Java - Practice Problems/Main.java
@@ -3,17 +3,21 @@ public class Main {
     public static void main(String[] args) {
 
         String s1 = stringExercise.parseString("<<>>", "word");
-        System.out.println(s1);
+        System.out.println("Q1: " + s1);
 
         String s2 = stringExercise.makeTags("i", "word");
-        System.out.println(s2);
+        System.out.println("Q2: " + s2);
 
         String s3 = stringExercise.withoutX2("xxwhy");
-        System.out.println(s3);
+        System.out.println("Q3: " + s3);
 
-        int[] tempArray = {1,2,3};
-        int[] a1 = arrayExercise.fix23(tempArray);
-        System.out.println(a1);
+        int[] a1Array = {1,2,3};
+        int[] a1 = arrayExercise.fix23(a1Array);
+        System.out.println("Q4: " + a1);
+
+        int[] a2Array = {1,2,3};
+        int[] a22Array = {1,3};
+        int a2 = arrayExercise.start1(a2Array, a22Array);
+        System.out.println("Q5: " + a2);
     }
 
-}
--- Java - Practice Problems/arrayExercise.java
+++ Java - Practice Problems/arrayExercise.java
@@ -27,6 +27,27 @@ public class arrayExercise {
         return intArray;
     }
 
+    //Start with 2 int arrays, a and b, of any length. Return how many of the arrays have 1 as their first element.
+    //start1([1, 2, 3], [1, 3]) → 2
+    //start1([1, 2], []) → 1
+    //Lesson: If a paramater being passed can be empty, you should FIRST check if the parameter is empty in the body or else you might get an arrayindexoutofboundsexception
+    public static int start1(int[] a, int[] b) {
+        int count = 0;
+
+        if (a.length == 0) { // This line does an empty array check
+            ; // This means if false, then do nothing
+        } else if (a[0] == 1) {
+            count++;
+        }
+
+        if (b.length == 0) {
+            ;
+        } else if (b[0] == 1) {
+            count++;
+        }
+
+        return count;
+    }
 
 
 
