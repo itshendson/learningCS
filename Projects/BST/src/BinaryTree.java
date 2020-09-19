9fcb7f62760cda25208b5be2780af0a875955a4f
Hendson <hendsonszeto@live.com>
2020-09-18 20:39:15 -0700

    trying to do BST

---
+++ Projects/BST/src/BinaryTree.java
@@ -0,0 +1,24 @@
+public class BinaryTree {
+
+    Node root;
+
+    public void addNode(int key, String name) {
+        Node newNode = new Node(key, name);
+
+        if (root == null) {
+            root = newNode;
+        } else {
+            Node focusNode = root;
+            Node parent;
+            }
+    }
+
+    public static void BinaryTree(String[] args) {
+
+
+
+
+
+
+    }
+}
