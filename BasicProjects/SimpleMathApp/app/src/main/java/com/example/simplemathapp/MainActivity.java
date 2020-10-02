c17ba39d2c22c3e5186a3eb690f755f1a8f3f61d
Hendson <hendsonszeto@live.com>
2020-10-02 12:05:04 -0700

    finished simple math app

--- BasicProjects/SimpleMathApp/app/src/main/java/com/example/simplemathapp/MainActivity.java
+++ BasicProjects/SimpleMathApp/app/src/main/java/com/example/simplemathapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.simplemathapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addButton = (Button) findViewById(R.id.addButton);
+        final EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+        final EditText secondNumEditText = (EditText) findViewById(R.id.SecondNumEditText);
+        final TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString()); //Convert the text in EditText then convert it to String then convert it to integer
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+
+                int result = num1 + num2;
+
+                resultTextView.setText(""+result);
+            }
+        });
     }
 }
