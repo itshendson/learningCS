60787fb72de208286b3c5a0f98b5e649ac15e317
Hendson <hendsonszeto@live.com>
2020-10-07 21:23:07 -0700

    trying to get list activity to work

--- BasicProjects/SimpleMathApp/app/src/main/java/com/example/simplemathapp/ListPage.java
+++ BasicProjects/SimpleMathApp/app/src/main/java/com/example/simplemathapp/ListPage.java
@@ -1,14 +1,28 @@
 package com.example.simplemathapp;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ListPage extends AppCompatActivity {
 
+    ListView myListView;
+    String[] items;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        Resources res = getResources();
+        myListView = (ListView) findViewById(R.id.myListView);
+        items = res.getStringArray(R.array.items); //This line refers to the items in strings.xml
+
+        //Merge items (which is an array of strings with myListView) by using what is called an 'adaptor'
+        myListView.setAdapter(new ArrayAdapter<String>(this, R.layout.my_listview_detail, items));
+
     }
 }
