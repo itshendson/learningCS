03ccc58268a94c9632d88c97f63b764cae21b523
Hendson <hendsonszeto@live.com>
2020-10-26 09:56:43 -0700

    interface basics

--- BasicProjects/AnotherInterfaceProject/src/TheFrame.java
+++ BasicProjects/AnotherInterfaceProject/src/TheFrame.java
@@ -5,16 +5,28 @@ public class TheFrame extends JFrame {
 
     TheFrame() {
 
-        this.setSize(400, 400); //sets the x and y dimensions of frame
+        //creating labels
+        JLabel label = new JLabel();
+        label.setText("The Start of Your Pokemon Journey");
+        ImageIcon kobe = new ImageIcon("C:\\Users\\Hendson-Desktop\\Desktop\\Personal\\Projects\\learningCS\\BasicProjects\\AnotherInterfaceProject\\src\\resources\\journey.jpg");
+        label.setIcon(kobe);
+        label.setHorizontalTextPosition(JLabel.CENTER); //set text left, center or right of imageicon
+        label.setVerticalTextPosition(JLabel.BOTTOM); //set text top, bottom of imageicon
+        label.setForeground(Color.BLACK);
+        label.setFont(new Font("MV Boli", Font.BOLD, 20));
+
+        //creating the frame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//When you click the x button, it will kill the application not just hide it
-        //frame.setResizable(false); //Prevents frame from being resized
+        this.setSize(500, 500); //sets the x and y dimensions of frame
         this.setTitle("Another Interface Project"); //Gives the frame window a name
         this.setVisible(true); //Makes frame visible
+        this.add(label);
 
-        ImageIcon image = new ImageIcon("spacex.png"); //Grabs the filename called "spacex" and set it to image
-        this.setIconImage(image.getImage());//Change icon of frame
-        //frame.getContentPane().setBackground(Color.gray); //change color of background
-        this.getContentPane().setBackground(new Color(255,100,150)); //more customizable colors
+        // Background color of frame
+        this.getContentPane().setBackground(Color.white); //change color of background
+        //this.getContentPane().setBackground(new Color(255,100,150)); //more customizable colors
+
+
 
     }
 }
