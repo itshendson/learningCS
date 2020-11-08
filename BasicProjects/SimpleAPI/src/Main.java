8074a1f6e385af2e7f8e1262717c43f18f669897
Hendson <hendsonszeto@live.com>
2020-11-08 00:49:17 -0800

    fixed type casting problem

--- BasicProjects/SimpleAPI/src/Main.java
+++ BasicProjects/SimpleAPI/src/Main.java
@@ -3,6 +3,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.io.Console;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -15,10 +16,12 @@ public class Main {
 
     public static void main(String args[]) {
 
+        Console console = System.console();
+
         String inline = "";
 
         try {
-            URL url = new URL("https://api.covid19api.com/country/south-africa/status/confirmed/live?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z"); // GET By Country Live
+            URL url = new URL("https://api.covid19api.com/summary"); // GET Summary by Country
             HttpURLConnection con = (HttpURLConnection) url.openConnection(); //Type cast url to HttpURLConnection object for benefits/behavior specific to this object
             con.setRequestMethod("GET");
             con.connect();
@@ -39,28 +42,24 @@ public class Main {
                 System.out.println("Response Format: JSON");
                 System.out.println(inline);
                 sc.close();
-
-                JSONParser parser = new JSONParser();
-                JSONObject jObjects = (JSONObject) parser.parse(inline); //Convert the string into JSON objects
-                JSONArray jArray_1 = (JSONArray) jObjects.get("Countries"); //Converts JSON object into JSONArray
-
-                for (int i = 0; i < jArray_1.size();i++) {
-                    JSONObject jsonObj_1 = (JSONObject) jArray_1.get(i);
-                    System.out.println("Country: " + jsonObj_1.get("Country"));
-                    System.out.println("Country: " + jsonObj_1.get("NewConfirmed"));
-                }
-
-
             }
 
+            JSONParser parser = new JSONParser();
+            JSONObject jObjects = (JSONObject) parser.parse(inline); //Convert the string into JSON objects
+            JSONArray jArray_1 = (JSONArray) jObjects.get("Countries"); //Converts JSON object into JSONArray
 
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
+            for (int i = 0; i < jArray_1.size();i++) {
+                JSONObject jsonObj_1 = (JSONObject) jArray_1.get(i);
+                if ((long) jsonObj_1.get("NewConfirmed") > 0) {
+                    System.out.println("\nCountry: " + jsonObj_1.get("Country"));
+                    System.out.println("New Confirmed: " + jsonObj_1.get("NewConfirmed"));
+                }
+            }
+
+            con.disconnect();
+            System.out.println("Disconnected");
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
