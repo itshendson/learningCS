67de872ff3211c7b3f128cd71ddc9c146f93c5a1
Hendson <hendsonszeto@live.com>
2021-01-03 00:34:01 -0800

    enable saving to local storage

--- Projects/MyCards/app.js
+++ Projects/MyCards/app.js
@@ -6,6 +6,7 @@ const cardList = document.querySelector('.todo-list');
 //Event Listeners
 cardButton.addEventListener('click', addCard);
 cardList.addEventListener('click', editOrDelete);
+document.addEventListener("DOMContentLoaded", loadCards);
 
 //Functions
 function addCard(event) {
@@ -21,6 +22,9 @@ function addCard(event) {
     cardLi.innerText = cardInput.value;
     cardLi.classList.add('card-item');
     cardDiv.appendChild(cardLi);
+
+    //Saves
+    saveLocal(cardInput.value);
 
     //Create Edit button
     const editButton = document.createElement('button');
@@ -40,11 +44,13 @@ function addCard(event) {
     cardInput.value = ""
 }
 
+//Function for Edit and Delete
 function editOrDelete(e) {
     const item = e.target;
     
     if (item.classList[0] === "trash-btn") {
         const cardParent = item.parentElement;
+        deleteLocalCard(cardParent);
         cardParent.remove();
     }
 
@@ -52,4 +58,68 @@ function editOrDelete(e) {
         console.log("Edit function implementation here")
     }
 
+}
+
+//Save function to local storage
+function saveLocal(card) {
+    
+    let cards;
+    if (localStorage.getItem("cards") === null) {
+        cards = [];
+    } else {
+        cards = JSON.parse(localStorage.getItem("cards"));
+    }
+    cards.push(card);
+    localStorage.setItem("cards", JSON.stringify(cards));
+}
+
+function loadCards() {
+    let cards;
+    if (localStorage.getItem("cards") === null) {
+        cards = [];
+    } else {
+        cards = JSON.parse(localStorage.getItem("cards"));
+    }
+    
+    cards.forEach(function(card) {
+        //Create <Div>
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('todo');
+
+        //Create <Li>
+        const cardLi = document.createElement('li');
+        cardLi.innerText = card;
+        cardLi.classList.add('card-item');
+        cardDiv.appendChild(cardLi);
+
+        //Create Edit button
+        const editButton = document.createElement('button');
+        editButton.innerHTML = '<i class="fas fa-pencil-alt"></i>';
+        editButton.classList.add("edit-btn");
+        cardDiv.appendChild(editButton);
+
+        //Create Delete button
+        const trashButton = document.createElement('button');
+        trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+        trashButton.classList.add("trash-btn");
+        cardDiv.appendChild(trashButton);
+
+        //Append the cardDiv
+        cardList.appendChild(cardDiv);
+    })
+
+}
+
+//Deletes element from LocalStorage array
+function deleteLocalCard(card) {
+    let cards;
+    if (localStorage.getItem("cards") === null) {
+        cards = [];
+    } else {
+        cards = JSON.parse(localStorage.getItem("cards"));
+    }
+    
+    const cardIndex = card.children[0].innerText;
+    cards.splice(cards.indexOf(cardIndex), 1);
+    localStorage.setItem("cards", JSON.stringify(cards));
 }
